35adfccf6f21ec34e7e3d33d697c01f6818794e1
xYoussefMahmoudx <youssef.mahmoud.eng@gmail.com>
2024-03-12 14:06:42 +0200

    add node and drag and alot of things

--- Automata_Converter/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.automata_converter {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.automata_converter to javafx.fxml;
-    exports com.example.automata_converter;
-}
