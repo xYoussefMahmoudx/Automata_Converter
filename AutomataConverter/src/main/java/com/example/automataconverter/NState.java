cdba681e0050bab28973cc11db752736ebf4a935
xYoussefMahmoudx <youssef.mahmoud.eng@gmail.com>
2024-05-07 11:47:48 +0300

    code refactored : removed unnecessary functions and code lines removed unused classes

--- AutomataConverter/src/main/java/com/example/automataconverter/NState.java
+++ AutomataConverter/src/main/java/com/example/automataconverter/NState.java
@@ -1,13 +1,11 @@
 package com.example.automataconverter;
 
 import callbackinterfaces.*;
-import javafx.beans.binding.Bindings;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-
 import java.util.ArrayList;
 
 
@@ -23,12 +21,10 @@ public class NState {
     private UpdateTransition updateTransition;
     private ShowTransitionScreen showTransitionScreen;
     private GetDestinationState getDestinationState;
-    ArrayList<STransition> STransitions = new ArrayList<STransition>();
-    STransition currentTransition;
-
+    private ArrayList<STransition> STransitions = new ArrayList<STransition>();
+    private STransition currentTransition;
 
     public NState(double radius,StateType stateType) {
-
         this.circle = new Circle(radius);
         this.stateType=stateType;
         innerCircle = new Circle();
@@ -55,31 +51,79 @@ public class NState {
             makeFinal();
         }
     }
-    public void setAnchorCallBack(RemoveNode callBack){
-        this.anchorPaneCallBack=callBack;
+    // External Use update Functions
+    public void updateTransitionLabel(){
+        currentTransition.setTiteral(this.TransitionCallBack.apply());
     }
-    public void setArrayCallBack(RemoveNodeFromArray callBack){
-        this.arrayCallBack=callBack;
+    public void updateDestination(){
+        currentTransition.setDestinationState(this.getDestinationState.apply());
     }
 
-    public void setUpdateTransition(UpdateTransition updateTransition) {
-        this.updateTransition = updateTransition;
-    }
-    public void setShowTransitionScreen(ShowTransitionScreen showTransitionScreen) {this.showTransitionScreen = showTransitionScreen;}
-
+    // Functional Functions
     private void createNode(){
         circle.setFill(Color.WHITE);
         circle.setStroke(Color.BLACK);
         circle.setStrokeWidth(2);
-        //this.stateType = StateType.Normal;
     }
+    private void makeFinal(){
+        innerCircle.setRadius(40); // Set smaller radius
+        innerCircle.setFill(Color.WHITE); // White fill
+        innerCircle.setStroke(Color.BLACK); // Black border
+        innerCircle.setStrokeWidth(2); // Border width
+        this.innerCircle.setOnMouseClicked(e->{
+            if(e.getButton() == MouseButton.SECONDARY){
+                sideMenu.getMenu().show(this.circle,e.getScreenX(),e.getScreenY());
+                e.consume();
+            }
+        });
+    }
+    private void addTransition(){
+        STransition transition =new STransition();
+        transition.setLine(new Line(this.circle.getCenterX()+ this.circle.getRadius(), this.circle.getCenterY() , this.circle.getCenterX()+this.circle.getRadius() + 20, this.circle.getCenterY() ));
+        double startX = this.circle.getCenterX()+50;
+        double startY=circle.getCenterY();
+        transition.getArrow().getPoints().addAll(
+                startX, startY - 20,
+                startX + 10, startY - 10,
+                startX, startY
 
-    public void setTransitionCallBack(AddTransition callBack){
-        this.TransitionCallBack=callBack;
+        );
+
+        updateTransition.apply().getChildren().add(transition.getArrow());
+        updateTransition.apply().getChildren().add(transition.getLine());
+        updateTransition.apply().getChildren().add(transition.getTliteral());
+        currentTransition=transition;
+        this.STransitions.add(transition);
+        this.circle.setOnMouseClicked(e->{
+            if(e.getButton() == MouseButton.SECONDARY){
+                sideMenu.getMenu().show(this.circle,e.getScreenX(),e.getScreenY());
+                e.consume();
+            }
+        });
     }
-    public void setGetDestinationState(GetDestinationState callBack){
-        this.getDestinationState=callBack;
+    private void makeNormal(){
+        innerCircle.setStroke(Color.TRANSPARENT); // Black border
     }
+    public void deleteNode(){
+        arrayCallBack.apply().remove(this);
+    }
+    //getters Functions
+    public Circle getCircle() {
+        return circle;
+    }
+    public Circle getInnerCircle() {
+        return innerCircle;
+    }
+    public RemoveNodeFromArray getArrayCallBack() {
+        return arrayCallBack;
+    }
+    public Label getStateName() {
+        return stateName;
+    }
+    public ArrayList<STransition> getTransitionSTransitions() {
+        return STransitions;
+    }
+    // onAction Event Functions
     private void onFinalClicked(){
         sideMenu.getfLabel().setOnMouseClicked(e-> {
             if (e.getButton() == MouseButton.PRIMARY) {
@@ -88,7 +132,6 @@ public class NState {
             }
         });
     }
-
     private void onNormalClicked(){
         sideMenu.getnLabel().setOnMouseClicked(e-> {
             if (e.getButton() == MouseButton.PRIMARY) {
@@ -105,7 +148,6 @@ public class NState {
             }
         });
     }
-
     private void onNodeDragged(){
         this.circle.setOnMouseClicked(e->{
             if(e.getButton() == MouseButton.SECONDARY){
@@ -114,7 +156,6 @@ public class NState {
             }
 
         });
-
 
         circle.setOnMouseDragged(event-> {
             circle.setCenterX(event.getX());
@@ -128,176 +169,66 @@ public class NState {
             stateName.setLayoutX(circle.getCenterX() - stateName.getWidth()/2);
             stateName.setLayoutY(circle.getCenterY() - stateName.getHeight()/2);
         });
-
     }
-
-    private void onTransitionClicked(){
-        sideMenu.gettLabel().setOnMouseClicked(e-> {
-            if (e.getButton() == MouseButton.PRIMARY) {
-                showTransitionScreen.apply();
-                addTransition();
-                onArrowDragged();
-
-            }
-        });
-
-
-    }
-
-    private void makeFinal(){
-        // Inner Circle (white fill)
-        innerCircle.setRadius(40); // Set smaller radius
-        innerCircle.setFill(Color.WHITE); // White fill
-        innerCircle.setStroke(Color.BLACK); // Black border
-        innerCircle.setStrokeWidth(2); // Border width
-        this.innerCircle.setOnMouseClicked(e->{
-            if(e.getButton() == MouseButton.SECONDARY){
-                sideMenu.getMenu().show(this.circle,e.getScreenX(),e.getScreenY());
-                e.consume();
-            }
-        });
-
-    }
-
-    private void addTransition(){
-        System.out.println(this.STransitions.size());
-
-        STransition transition =new STransition();
-        transition.setLine(new Line(this.circle.getCenterX()+ this.circle.getRadius(), this.circle.getCenterY() , this.circle.getCenterX()+this.circle.getRadius() + 20, this.circle.getCenterY() ));
-
-            double startX = this.circle.getCenterX()+50;
-            double startY=circle.getCenterY();
-
-
-
-        transition.getArrow().getPoints().addAll(
-                    startX, startY - 20,
-                    startX + 10, startY - 10,
-                    startX, startY
-
-            );
-
-
-
-
-        updateTransition.apply().getChildren().add(transition.getArrow());
-        updateTransition.apply().getChildren().add(transition.getLine());
-        updateTransition.apply().getChildren().add(transition.getTliteral());
-
-
-        currentTransition=transition;
-        this.STransitions.add(transition);
-
-
-        this.circle.setOnMouseClicked(e->{
-            if(e.getButton() == MouseButton.SECONDARY){
-                sideMenu.getMenu().show(this.circle,e.getScreenX(),e.getScreenY());
-                e.consume();
-            }
-        });
-
-        //this.line.setStroke(Color.RED);
-    }
-
-
-
-    private void makeNormal(){
-        innerCircle.setStroke(Color.TRANSPARENT); // Black border
-    }
-
-    public Circle getCircle() {
-        return circle;
-    }
-
-    public Circle getInnerCircle() {
-        return innerCircle;
-    }
-
-
-    public RemoveNodeFromArray getArrayCallBack() {
-        return arrayCallBack;
-    }
-
-    public void deleteNode(){
-        arrayCallBack.apply().remove(this);
-    }
-
-
-    public Label getStateName() {
-        return stateName;
-    }
-
-    public void setStateName(String stateName) {
-        this.stateName.setText(stateName);
-    }
-
     private void onArrowDragged() {
-        System.out.println("i am here before  draged thing"+this.STransitions.size());
-
         for(STransition transition : this.STransitions) {
-            System.out.println(transition.getTliteral().getText());
-
-
             transition.getArrow().setOnMouseDragged(event -> {
                 transition.getArrow().getPoints().setAll(
                         event.getX(), event.getY() - 20,
                         event.getX() + 10, event.getY() - 10,
                         event.getX(), event.getY()
                 );
-
-
                 transition.getLine().setFill(Color.RED);
                 transition.getLine().setStartX(this.circle.getCenterX() + 50);
                 transition.getLine().setStartY(circle.getCenterY());
                 transition.getLine().setEndX(event.getX());
                 transition.getLine().setEndY(event.getY() - 10);
-               // transition.getTliteral().layoutXProperty().bind((transition.getLine().startXProperty().add(transition.getLine().endXProperty())).divide(2));
-               // transition.getTliteral().layoutYProperty().bind((transition.getLine().startYProperty().add(transition.getLine().endYProperty())).divide(2));
                 transition.getTliteral().translateXProperty().bind((transition.getLine().startXProperty().add(transition.getLine().endXProperty())).divide(2).subtract(transition.getTliteral().widthProperty().divide(2)));
                 // Bind label's translate Y to the midpoint of the line's start and end Y minus an offset
                 transition.getTliteral().translateYProperty().bind((transition.getLine().startYProperty().add(transition.getLine().endYProperty())).divide(2).subtract(20));
-
-
                 double startX = transition.getDestinationState().getCircle().getCenterX()+50;
                 double startY=transition.getDestinationState().getCircle().getCenterY();
-
-
-                        transition.getArrow().getPoints().addAll(
-                                startX+50, startY - 20,
-                                startX + 10, startY - 10,
-                                startX, startY
-                        );
+                transition.getArrow().getPoints().addAll(
+                        startX+50, startY - 20,
+                        startX + 10, startY - 10,
+                        startX, startY
+                );
                 double angle = Math.atan2(transition.getDestinationState().circle.getCenterY() - this.circle.getCenterY(), transition.getDestinationState().circle.getCenterX() - this.circle.getCenterX());
                 double endX = transition.getDestinationState().circle.getCenterX() - transition.getDestinationState().circle.getRadius() * Math.cos(angle);
                 double endY = transition.getDestinationState().circle.getCenterY() - transition.getDestinationState().circle.getRadius() * Math.sin(angle);
-
-                // Create line
-               // currentTransition.setLine(new Line(transition.getDestinationState().circle.getCenterX()+ transition.getDestinationState().circle.getRadius(), transition.getDestinationState().circle.getCenterY() , transition.getDestinationState().circle.getCenterX()+transition.getDestinationState().circle.getRadius() + 20, transition.getDestinationState().circle.getCenterY() ));
-
-
-
             });
-
-
         }
     }
+    private void onTransitionClicked(){
+        sideMenu.gettLabel().setOnMouseClicked(e-> {
+            if (e.getButton() == MouseButton.PRIMARY) {
+                showTransitionScreen.apply();
+                addTransition();
+                onArrowDragged();
+            }
+        });
 
-
-    public ArrayList<STransition> getTransitionSTransitions() {
-        return STransitions;
     }
-
-    public void updateTransitionLabel(){
-        currentTransition.setTiteral(this.TransitionCallBack.apply());
+    // setters for the callBacks
+    public void setAnchorCallBack(RemoveNode callBack){
+        this.anchorPaneCallBack=callBack;
     }
-
-    public void updateDestination(){
-        currentTransition.setDestinationState(this.getDestinationState.apply());
+    public void setArrayCallBack(RemoveNodeFromArray callBack){
+        this.arrayCallBack=callBack;
     }
-    public void printDest(){
-        for(STransition transition : this.STransitions) {
-            System.out.println(transition.getDestinationState().getStateName().getText());
-        }
+    public void setUpdateTransition(UpdateTransition updateTransition) {
+        this.updateTransition = updateTransition;
+    }
+    public void setShowTransitionScreen(ShowTransitionScreen showTransitionScreen) {this.showTransitionScreen = showTransitionScreen;}
+    public void setTransitionCallBack(AddTransition callBack){
+        this.TransitionCallBack=callBack;
+    }
+    public void setGetDestinationState(GetDestinationState callBack){
+        this.getDestinationState=callBack;
+    }
+    //setter Functions
+    public void setStateName(String stateName) {
+        this.stateName.setText(stateName);
     }
 }
 
